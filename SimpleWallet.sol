5bd76610643a08952e189cd40b3b7a973818c715
Korges <korgesik@gmail.com>
2022-06-18 13:49:11 +0200

    Emit events

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable {
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
+
     mapping(address => uint) public allowance;
 
     function addAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
     function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
 
@@ -22,11 +26,15 @@ contract Allowance is Ownable {
 
 contract SimpleWallet is Allowance {
 
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require(_amount <= address(this).balance, "There are not anough funds stored in the smart contract!");
         if(owner() != _msgSender()) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
@@ -34,8 +42,8 @@ contract SimpleWallet is Allowance {
         return address(this).balance;
     }
 
-    receive() external payable {
-        
+    receive () external payable {
+        emit MoneyReceived(msg.sender, msg.value);
     }
 
 }
